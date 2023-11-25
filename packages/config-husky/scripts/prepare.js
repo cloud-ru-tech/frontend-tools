bc15ede92b542c4fef953163f2e5118af67f70eb
yetihead <yetibrain@yandex.ru>
2023-11-25 21:45:26 +0300

    fix(FF-0000): husky postinstall script

--- packages/config-husky/scripts/prepare.js
+++ packages/config-husky/scripts/prepare.js
@@ -2,15 +2,17 @@ const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-execSync(`cd ${process.env.INIT_CWD} && husky install`);
+const root = path.resolve(process.env.INIT_CWD);
 
-!fs.existsSync(path.resolve(process.env.INIT_CWD, '.husky/pre-commit')) &&
-  execSync(`cd ${process.env.INIT_CWD} && husky set .husky/pre-commit node_modules/.bin/lint-staged`);
+execSync(`cd ${root} && npx husky install`);
 
-!fs.existsSync(path.resolve(process.env.INIT_CWD, '.husky/pre-push')) &&
-  execSync(`cd ${process.env.INIT_CWD} && husky set .husky/pre-push node_modules/.bin/solidarity`);
+!fs.existsSync(path.resolve(root, '.husky/pre-commit')) &&
+  execSync(`cd ${root} && npx husky set .husky/pre-commit node_modules/.bin/lint-staged`);
 
-!fs.existsSync(path.resolve(process.env.INIT_CWD, '.husky/commit-msg')) &&
-  execSync(`cd ${process.env.INIT_CWD} && husky set .husky/commit-msg node_modules/.bin/cloud-ru-commit-message`);
+!fs.existsSync(path.resolve(root, '.husky/pre-push')) &&
+  execSync(`cd ${root} && npx husky set .husky/pre-push node_modules/.bin/solidarity`);
 
-execSync(`cp -f ${path.resolve(__dirname, '.solidarity')} ${path.resolve(process.env.INIT_CWD, '.solidarity')}`);
+!fs.existsSync(path.resolve(root, '.husky/commit-msg')) &&
+  execSync(`cd ${root} && npx husky set .husky/commit-msg node_modules/.bin/cloud-ru-commit-message`);
+
+fs.cpSync(path.resolve(__dirname, '.solidarity'), path.resolve(root, '.solidarity'), { recursive: true });
