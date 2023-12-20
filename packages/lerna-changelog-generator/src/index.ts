b84c61f356d7b51d863d7aa0b635669e1a8c1422
Sergei Khlupin <yetibrain@yandex.ru>
2023-12-20 13:51:50 +0300

    feat(FF-3958): add projectUrl and defaultBranch arguments

--- packages/lerna-changelog-generator/src/index.ts
+++ packages/lerna-changelog-generator/src/index.ts
@@ -4,6 +4,8 @@ import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
 import simpleGit from 'simple-git';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
 type Package = {
   packagePath: string;
@@ -13,12 +15,16 @@ type Package = {
   changedDeps: { packageName: string; packagePath: string }[];
 };
 
+type WriteChangesOptions = {
+  packages: Package[];
+  currentProjectUrl: string;
+  defaultBranch: string;
+};
+
 const git = simpleGit();
 
 const commitMessage = '[ci skip] Version and changelog releases';
 const changelogFile = 'CHANGELOG.md';
-const currentProjectUrl = process.env['CI_PROJECT_URL'] || '';
-const defaultBranch = process.env['CI_DEFAULT_BRANCH'] || '';
 
 const generateConventionalCommitsChangelog = () => {
   const lernaParameters = [
@@ -72,7 +78,7 @@ const findChangedDependencies = (allFiles: string[]) => {
   return changedPackages.map(findDependencies(changedPackages));
 };
 
-const writeChangedDependencies = (packages: Package[]) => {
+const writeChangedDependencies = ({ packages, currentProjectUrl, defaultBranch }: WriteChangesOptions) => {
   const changedDepsHeader = '### Only dependencies have been changed\n';
   const replaceText = '**Note:** Version bump only for package';
 
@@ -84,7 +90,7 @@ const writeChangedDependencies = (packages: Package[]) => {
     const depsString = pkg.changedDeps
       .map(({ packageName, packagePath }) => {
         // eslint-disable-next-line
-        const pathToChangelog = join(currentProjectUrl, '/-/blob/', defaultBranch, packagePath, changelogFile).replace(
+        const pathToChangelog = join(currentProjectUrl, '/blob/', defaultBranch, packagePath, changelogFile).replace(
           ':/',
           '://',
         );
@@ -94,6 +100,19 @@ const writeChangedDependencies = (packages: Package[]) => {
     const newFileData = fileData.replace(`${replaceText} ${pkg.packageName}`, `${changedDepsHeader}${depsString}`);
     writeFileSync(changelogPath, newFileData);
   });
+};
+
+const getArgs = async (): Promise<{ projectUrl: string; defaultBranch: string }> => {
+  try {
+    const argv = await yargs(hideBin(process.argv)).argv;
+
+    return {
+      projectUrl: typeof argv.projectUrl === 'string' ? argv.projectUrl : '',
+      defaultBranch: typeof argv.defaultBranch === 'string' ? argv.defaultBranch : '',
+    };
+  } catch (e) {
+    return { projectUrl: '', defaultBranch: '' };
+  }
 };
 
 const generateChangelog = async () => {
@@ -106,8 +125,14 @@ const generateChangelog = async () => {
     console.warn('No new versions have been released');
     return 'false';
   }
+
+  const { projectUrl, defaultBranch } = await getArgs();
   const packagesWithChangedDeps = findChangedDependencies(allFiles);
-  writeChangedDependencies(packagesWithChangedDeps);
+  writeChangedDependencies({
+    packages: packagesWithChangedDeps,
+    currentProjectUrl: projectUrl || process.env['CI_PROJECT_URL'] || '',
+    defaultBranch: defaultBranch || process.env['CI_DEFAULT_BRANCH'] || '',
+  });
 
   await git.checkout(filesToRestore);
   await git.add('./*').commit(commitMessage);
