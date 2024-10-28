5cde1ade16aa9c693ed879d96108f1f8f48db1b9
Белов Алексей <anbelov@cloud.ru>
2024-10-28 17:23:11 +0400

    fix(FF-5708): error: Cannot modify immutable object, remove unnecessary file

--- packages/conventional-changelog/conventional-changelog.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env node */
-const Q = require('q');
-const parserOpts = require('./parser-opts');
-const writerOpts = require('./writer-opts');
-
-module.exports = Q.all([parserOpts, writerOpts]).spread((parserOpts, writerOpts) => ({ parserOpts, writerOpts }));
