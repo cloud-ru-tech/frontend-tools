8b0c2092fa1a140fdde61c0547a90e43e679aba6
Anna Kozlova <fyz95@mail.ru>
2024-10-29 19:05:42 +0300

    fix!: forbid important, allow z-index inital, provide better messages for color props

--- packages/config-stylelint/index.js
+++ packages/config-stylelint/index.js
@@ -1,15 +1,31 @@
 module.exports = {
   rules: {
-    'color-no-hex': true,
-    'function-disallowed-list': ['/^rgb/'],
-    'color-named': 'never',
-    'property-disallowed-list': [
-      ['z-index'],
+    'color-no-hex': [
+      true,
+      { severity: 'error', message: `Don't use hex colors like HEX, you should use token instead` },
+    ],
+    'function-disallowed-list': [
+      ['/^rgb$/', '/^rgba$/', '/^hsl$/', '/^hwb$/', '/^lab$/', '/^lch$/', '/^color&/', '/^oklab$/', '/^oklch$/'],
+      {
+        severity: 'error',
+        message: `Don't use color functions like rgb, rgba, hsl and etc., you should use token instead`,
+      },
+    ],
+    'color-named': [
+      'never',
+      {
+        severity: 'error',
+        message: `Don't use named colors, you should use token instead`,
+      },
+    ],
+    'declaration-property-value-allowed-list': [
+      { 'z-index': ['initial'] },
       {
         message:
           'Property "z-index" is restricted from use. Docs: https://github.com/cloud-ru-tech/frontend-tools/tree/master/packages/config-stylelint#z-index',
       },
     ],
+    'declaration-no-important': true,
   },
   overrides: [
     {
